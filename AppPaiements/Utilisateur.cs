0d23909510d00892842ea190cf28017163b67b26
ALI ECH-CHARIF <300135851@monboreal.ca>
2024-10-24 15:19:48 -0400

    Ajout de la classe Utilisateur

---
+++ AppPaiements/Utilisateur.cs
@@ -0,0 +1,20 @@
+﻿public class Utilisateur
+{
+    public string Nom { get; set; }
+    public string Prenom { get; set; }
+    public string Email { get; set; }
+
+    // Constructeur de la classe Utilisateur
+    public Utilisateur(string nom, string prenom, string email)
+    {
+        Nom = nom;
+        Prenom = prenom;
+        Email = email;
+    }
+
+    // Méthode pour afficher les détails de l'utilisateur
+    public void AfficherDetails()
+    {
+        Console.WriteLine($"Nom : {Nom}, Prénom : {Prenom}, Email : {Email}");
+    }
+}
