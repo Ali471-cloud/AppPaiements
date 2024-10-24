88ffb0564a2d7561bc6daef6f1744a90b891e138
ALI ECH-CHARIF <300135851@monboreal.ca>
2024-10-24 15:57:13 -0400

    Ajout de la classe Produit

---
+++ AppPaiements/Produit.cs
@@ -0,0 +1,16 @@
+﻿public class Produit
+{
+    public string Nom { get; set; }
+    public double Prix { get; set; }
+
+    public Produit(string nom, double prix)
+    {
+        Nom = nom;
+        Prix = prix;
+    }
+
+    public void AfficherDetails()
+    {
+        Console.WriteLine($"Produit : {Nom}, Prix : {Prix}");
+    }
+}
