a6b2771a3390414306b4879a6d786494e7588eec
ALI ECH-CHARIF <300135851@monboreal.ca>
2024-10-24 12:16:46 -0400

    Ajout de Paiement.cs

---
+++ AppPaiements/Paiement.cs
@@ -0,0 +1,15 @@
+﻿public abstract class Paiement
+{
+    public double Montant { get; set; }
+    public string Description { get; set; }
+
+    // Constructeur qui prend le montant et la description en paramètres
+    public Paiement(double montant, string description)
+    {
+        Montant = montant;
+        Description = description;
+    }
+
+    // Méthode abstraite qui devra être implémentée par les classes filles
+    public abstract void AfficherDetails();
+}
